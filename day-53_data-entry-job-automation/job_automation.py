928f9ca1560caab150c6bdee9b64c02b2358784a
Steve Nhan <nhann.steve@gmail.com>
2025-01-07 00:02:34 -0500

    day 53: complete

--- day-53_data-entry-job-automation/job_automation.py
+++ day-53_data-entry-job-automation/job_automation.py
@@ -6,8 +6,10 @@ from dotenv import load_dotenv
 import requests
 from bs4 import BeautifulSoup
 
+
 class Job_Automation:
     load_dotenv()
+
     def __init__(self):
         self.scrap_data()
         self.populate_form()
@@ -19,7 +21,8 @@ class Job_Automation:
 
         # Get data from zillow
         self.link_list = [link['href'] for link in soup.select('a[class="property-card-link"]')]
-        self.price_list = [price.getText().split("+")[0].split("/")[0] for price in soup.select('span[data-test="property-card-price"]')]
+        self.price_list = [price.getText().split("+")[0].split("/")[0] for price in
+                           soup.select('span[data-test="property-card-price"]')]
         self.address_list = [price.getText().strip().split("|")[-1].strip() for price in soup.find_all('address')]
 
     def populate_form(self):
@@ -28,18 +31,19 @@ class Job_Automation:
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get(os.getenv("GOOGLEFORM"))
 
-        # todo find element for the 3 inputs
-        # todo submit and repeat with next input
-
         for n in range(len(self.link_list)):
-            address_input = self.driver.find_element(by=By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
-            address_input.send_keys(self.address_list[n])
-            price_input = self.driver.find_element(by=By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
-            address_input.send_keys(self.address_list[n])
-            address_input = self.driver.find_element(by=By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
-            address_input.send_keys(self.address_list[n])
+            input_elements = self.driver.find_elements(by=By.CSS_SELECTOR, value='input[type="text"]')
+
+            input_elements[0].send_keys(self.address_list[n])
+            input_elements[1].send_keys(self.price_list[n])
+            input_elements[2].send_keys(self.link_list[n])
 
             print(f"{self.link_list[n]} {self.price_list[n]} {self.address_list[n]}")
+            submit_button = self.driver.find_element(by=By.CSS_SELECTOR, value='span[class="NPEfkd RveJvd snByac"]')
+            submit_button.click()
+
+            next_response = self.driver.find_element(by=By.LINK_TEXT, value="Submit another response")
+            next_response.click()
 
 
 auto = Job_Automation()
